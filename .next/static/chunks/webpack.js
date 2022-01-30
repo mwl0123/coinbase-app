83cab368f1716d0e380574308bff9077cf6541bc
mwl0123 <mwl01232outlook.com>
2022-01-30 21:58:26 +0800

    add coin template

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2f96b7c88710619"; }
+/******/ 		__webpack_require__.h = function() { return "f9d96df20bc462a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
